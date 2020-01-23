250abeb341c5030c67ec28fafcedbe897ea882b2
asphum <asphum@gmail.com>
2020-01-23 19:42:39 +0700

    init

---
+++ .next/static/webpack/static/development/pages/index.js.a617d6de11d136beb48d.hot-update.js
@@ -0,0 +1,21 @@
+webpackHotUpdate("static\\development\\pages\\index.js",{
+
+/***/ "./node_modules/react/index.js":
+false,
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+
+
+/***/ }),
+
+/***/ "dll-reference dll_ef0ff7c60362f24a921f":
+false
+
+})
+//# sourceMappingURL=index.js.a617d6de11d136beb48d.hot-update.js.map
